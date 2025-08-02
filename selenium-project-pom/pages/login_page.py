3f9eeb66c7f96ae9e14c09cc6f711e095c668b20
msilva.yaman <msilva.yaman@bancomaster.com.br>
2025-08-02 18:03:15 -0300

    Organizando locators de login

--- selenium-project-pom/pages/login_page.py
+++ selenium-project-pom/pages/login_page.py
@@ -5,8 +5,11 @@ class LoginPage:
     # construtor da classe
     def __init__(self):
         self.driver = conftest.driver
+        self.username_field = (By.XPATH, "//input[@id='user-name']")
+        self.password_field = (By.XPATH, "//input[@id='password']")
+        self.login_button = (By.XPATH, "//input[@id='login-button']")
 
     def fazer_login(self, usuario, senha):
-        self.driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys(usuario)
-        self.driver.find_element(By.XPATH, "//input[@id='password']").send_keys(senha)
-        self.driver.find_element(By.XPATH, "//input[@id='login-button']").click()
+        self.driver.find_element(*self.username_field).send_keys(usuario)
+        self.driver.find_element(*self.password_field).send_keys(senha)
+        self.driver.find_element(*self.login_button).click()
