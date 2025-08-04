f2bd4eedc50dbb268d3e063b8a8847b6c7546796
msilva.yaman <msilva.yaman@bancomaster.com.br>
2025-08-04 18:48:40 -0300

    [REFECTOR] Refatorando login inválido

--- selenium-project-pom/pages/login_page.py
+++ selenium-project-pom/pages/login_page.py
@@ -9,6 +9,7 @@ class LoginPage(BasePage):
         self.username_field = (By.XPATH, "//input[@id='user-name']")
         self.password_field = (By.XPATH, "//input[@id='password']")
         self.login_button = (By.XPATH, "//input[@id='login-button']")
+        self.error_message_login = (By.XPATH, "//*[@class='error-message-container error']")
 
     def fazer_login(self, usuario, senha):
         # self.driver.find_element(*self.username_field).send_keys(usuario)
@@ -17,5 +18,8 @@ class LoginPage(BasePage):
         self.escrever(self.username_field, usuario)
         self.escrever(self.password_field, senha)
         self.clicar(self.login_button)
+
+    def verificar_mensagem_erro_login(self):
+        self.verificar_se_elemento_existe(self.error_message_login)
 
 
--- selenium-project-pom/tests/test_ct04_desafio_validar_login_com_senha_invalida.py
+++ selenium-project-pom/tests/test_ct04_desafio_validar_login_com_senha_invalida.py
@@ -1,14 +1,14 @@
 import pytest
 from selenium.webdriver.common.by import By
-import conftest
+
+from pages.login_page import LoginPage
+
 
 @pytest.mark.usefixtures("setup_teardown")
 @pytest.mark.login
 class TestCT04:
     def test_ct04_desafio_validar_login_com_senha_invalida(self):
-        driver = conftest.driver
-        driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys("standard_user")
-        driver.find_element(By.XPATH, "//input[@id='password']").send_keys("secret_sauce99")
-        driver.find_element(By.XPATH, "//input[@id='login-button']").click()
-        # validar mensagem de credenciais inválidas
-        assert driver.find_element(By.XPATH, "//*[@class='error-message-container error']").is_displayed()
+        login_page = LoginPage()
+        login_page.fazer_login("standard_user","senha_invalida")
+
+        login_page.verificar_mensagem_erro_login()
